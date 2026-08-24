5b7d69af3791c26728ba36aca9f17312e030cd77
KaihuiLiang <cafe.liang@gmail.com>
2026-08-24 18:47:58 +0000

    Deploying to gh-pages from @ YixiC94/YixiC94.github.io@d67d9e31a0bbb898bb5715c600e882ced46e8f1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,12 +4,19 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
+  },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
+        },{id: "nav-cv",
           title: "CV",
           description: "",
           section: "Navigation",
